c4870c7849f7b99b6dc6d2c5cd82cb0874247259
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2020-12-09 00:56:21 -0800

    day 9 part 1

---
+++ cmd/day9/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	input, err := getInput()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Part 1: %d\n", part1(input))
+}
+
+func part1(input []int) int {
+	var preceding [25]int
+
+	for i := 0; i < 25; i++ {
+		preceding[i] = input[i]
+	}
+
+	for i := 25; i < len(input); i++ {
+		matchFound := false
+		for j := 0; j < len(preceding); j++ {
+			if preceding[j] > input[i] {
+				continue
+			}
+			for k := 0; k < len(preceding); k++ {
+				if j == k {
+					continue
+				}
+				if preceding[j]+preceding[k] == input[i] {
+					matchFound = true
+					break
+				}
+			}
+			if matchFound {
+				break
+			}
+		}
+
+		if !matchFound {
+			return input[i]
+		}
+		preceding[i%25] = input[i]
+	}
+
+	return -1
+}
+
+func getInput() ([]int, error) {
+	f, err := os.Open("inputs/day9.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]int, 0)
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		item, err := strconv.Atoi(s.Text())
+		if err != nil {
+			return result, err
+		}
+
+		result = append(result, item)
+	}
+
+	return result, nil
+}
