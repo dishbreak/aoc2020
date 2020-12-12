b5e43e53ff51b98448f0d66709b412375c46c023
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2020-12-11 22:08:43 -0800

    tests pass

--- cmd/day11/main.go
+++ cmd/day11/main.go
@@ -18,34 +18,22 @@ func main() {
 
 // adapted from https://golang.org/doc/play/life.go
 type Field struct {
-	s          [][]bool
+	s          [][]int
 	rows, cols int
-	f          [][]bool
 }
 
-func NewField(rows, cols int, input []string) *Field {
-	s := make([][]bool, rows)
+func NewField(rows, cols int) *Field {
+	s := make([][]int, rows)
 	for i := range s {
-		s[i] = make([]bool, cols)
+		s[i] = make([]int, cols)
 	}
 
-	f := make([][]bool, rows)
-	for i := range f {
-		f[i] = make([]bool, cols)
-	}
-
-	for i, row := range input {
-		for j, col := range row {
-			f[i][j] = col == '.'
-		}
-	}
-
-	return &Field{s: s, f: f, rows: rows, cols: cols}
+	return &Field{s: s, rows: rows, cols: cols}
 }
 
-func (f *Field) IsFilled(row, col int) bool {
+func (f *Field) IsFilled(row, col int) int {
 	if row < 0 || col < 0 || row >= f.rows || col >= f.cols {
-		return false
+		return 0
 	}
 	return f.s[row][col]
 }
@@ -64,9 +52,7 @@ func (f *Field) FilledNeighbors(row, col int) int {
 	}
 
 	for _, point := range neighborCoords {
-		if f.IsFilled(point[0], point[1]) {
-			filledNeigbhors++
-		}
+		filledNeigbhors += f.IsFilled(point[0], point[1])
 	}
 
 	return filledNeigbhors
@@ -75,17 +61,29 @@ func (f *Field) FilledNeighbors(row, col int) int {
 type Game struct {
 	a          *Field
 	b          *Field
+	floor      *Field
 	rows, cols int
 }
 
 func NewGame(input []string) *Game {
 	rows := len(input)
 	cols := len(input[0])
+
+	floor := NewField(rows, cols)
+	for i, row := range input {
+		for j, col := range row {
+			if col == '.' {
+				floor.s[i][j] = 1
+			}
+		}
+	}
+
 	return &Game{
-		rows: rows,
-		cols: cols,
-		a:    NewField(rows, cols, input),
-		b:    NewField(rows, cols, input),
+		rows:  rows,
+		cols:  cols,
+		a:     NewField(rows, cols),
+		b:     NewField(rows, cols),
+		floor: floor,
 	}
 }
 
@@ -93,15 +91,15 @@ func (g *Game) Increment() bool {
 	changed := false
 	for i, row := range g.a.s {
 		for j := range row {
-			if g.a.f[i][j] {
+			if g.floor.s[i][j] == 1 {
 				continue
 			}
 			neighbors := g.a.FilledNeighbors(i, j)
-			if g.a.s[i][j] && neighbors >= 4 {
-				g.b.s[i][j] = false
+			if g.a.s[i][j] == 1 && neighbors >= 4 {
+				g.b.s[i][j] = 0
 				changed = true
-			} else if !g.a.s[i][j] && neighbors == 0 {
-				g.b.s[i][j] = true
+			} else if g.a.s[i][j] == 0 && neighbors == 0 {
+				g.b.s[i][j] = 1
 				changed = true
 			} else {
 				g.b.s[i][j] = g.a.s[i][j]
@@ -118,9 +116,7 @@ func (g *Game) OccupiedSeats() int {
 	occupied := 0
 	for i, row := range g.a.s {
 		for j := range row {
-			if g.a.s[i][j] {
-				occupied++
-			}
+			occupied += g.a.s[i][j]
 		}
 	}
 	return occupied
@@ -129,9 +125,9 @@ func (g *Game) String() string {
 	var b bytes.Buffer
 	for i, row := range g.a.s {
 		for j := range row {
-			if g.a.f[i][j] {
+			if g.floor.s[i][j] == 1 {
 				b.WriteByte('.')
-			} else if g.a.s[i][j] {
+			} else if g.a.s[i][j] == 1 {
 				b.WriteByte('#')
 			} else {
 				b.WriteByte('L')
