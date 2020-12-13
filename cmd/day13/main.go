1a8bc8c7463e6761e200d73fb85678f85c746412
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2020-12-13 14:45:03 -0800

    day 13 part 1 tests pass

--- cmd/day13/main.go
+++ cmd/day13/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
 
 func main() {
-	input, err := lib.GetInput("inputs/dayNN.txt")
+	input, err := lib.GetInput("inputs/day13.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -16,8 +18,60 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
+type BusCalculator struct {
+	startTime int
+	busIds    []int
+	maxId     int
+}
+
+func NewBusCalculator(input []string) (*BusCalculator, error) {
+	startTime, err := strconv.Atoi(input[0])
+	if err != nil {
+		return nil, err
+	}
+
+	maxId := 0
+	busIds := make([]int, 0)
+	for _, id := range strings.Split(input[1], ",") {
+		if id == "x" {
+			continue
+		}
+		if parsed, err := strconv.Atoi(id); err == nil {
+			if maxId < parsed {
+				maxId = parsed
+			}
+			busIds = append(busIds, parsed)
+		} else {
+			return nil, err
+		}
+	}
+
+	return &BusCalculator{
+		startTime: startTime,
+		busIds:    busIds,
+		maxId:     maxId,
+	}, nil
+}
+
+func (b *BusCalculator) FindNextBus() (int, int) {
+	for i := b.startTime; i <= b.startTime+b.maxId; i++ {
+		for _, busId := range b.busIds {
+			if i%busId == 0 {
+				return busId, i - b.startTime
+			}
+		}
+	}
+	return -1, -1
+}
+
 func part1(input []string) int {
-	return 0
+	calc, err := NewBusCalculator(input)
+	if err != nil {
+		panic(err)
+	}
+
+	id, timeToBus := calc.FindNextBus()
+	return id * timeToBus
 }
 
 func part2(input []string) int {
--- cmd/day13/main_test.go
+++ cmd/day13/main_test.go
@@ -6,10 +6,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{}
+var input = []string{
+	"939",
+	"7,13,x,x,59,x,31,19",
+}
+
+func TestNewBusCalculator(t *testing.T) {
+	calc, err := NewBusCalculator(input)
+	assert.Nil(t, err)
+	assert.Equal(t, &BusCalculator{
+		startTime: 939,
+		busIds:    []int{7, 13, 59, 31, 19},
+		maxId:     59,
+	}, calc)
+}
 
 func TestPart1(t *testing.T) {
-	assert.Equal(t, 0, part1(input))
+	assert.Equal(t, 295, part1(input))
 }
 
 func TestPart2(t *testing.T) {
