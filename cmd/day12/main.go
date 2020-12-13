23839ef8f3fb2f552810547f1a8ab19717964422
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2020-12-13 13:31:32 -0800

    day 12 part 2

--- cmd/day12/main.go
+++ cmd/day12/main.go
@@ -15,6 +15,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 var headingVectors = []image.Point{
@@ -70,9 +71,71 @@ func (s *Ship) ManhattanDist() int {
 	return abs(s.position.X) + abs(s.position.Y)
 }
 
+type ShipWithBearing struct {
+	position      image.Point
+	bearingVector image.Point
+}
+
+func (s *ShipWithBearing) GiveInstruction(instruction string) {
+	if len(instruction) < 2 {
+		return
+	}
+
+	arg, err := strconv.Atoi(string(instruction[1:]))
+	if err != nil {
+		panic(err)
+	}
+
+	switch instruction[0] {
+	case 'N':
+		vector := headingVectors[0].Mul(arg)
+		s.bearingVector = s.bearingVector.Add(vector)
+	case 'E':
+		s.bearingVector = s.bearingVector.Add(headingVectors[1].Mul(arg))
+	case 'S':
+		s.bearingVector = s.bearingVector.Add(headingVectors[2].Mul(arg))
+	case 'W':
+		s.bearingVector = s.bearingVector.Add(headingVectors[3].Mul(arg))
+	case 'R':
+		s.rotateRight(arg)
+	case 'L':
+		s.rotateLeft(arg)
+	case 'F':
+		s.position = s.position.Add(s.bearingVector.Mul(arg))
+	}
+}
+
+func (s *ShipWithBearing) rotateLeft(degrees int) {
+	for i := 0; i < degrees/90; i++ {
+		s.bearingVector.X, s.bearingVector.Y = -1*s.bearingVector.Y, s.bearingVector.X
+	}
+}
+
+func (s *ShipWithBearing) rotateRight(degrees int) {
+	for i := 0; i < degrees/90; i++ {
+		s.bearingVector.X, s.bearingVector.Y = s.bearingVector.Y, -1*s.bearingVector.X
+	}
+}
+
+func (s *ShipWithBearing) ManhattanDist() int {
+	return abs(s.position.X) + abs(s.position.Y)
+}
+
 func part1(input []string) int {
 	s := &Ship{
 		heading: 90,
+	}
+
+	for _, instruction := range input {
+		s.GiveInstruction(instruction)
+	}
+
+	return s.ManhattanDist()
+}
+
+func part2(input []string) int {
+	s := &ShipWithBearing{
+		bearingVector: image.Point{10, 1},
 	}
 
 	for _, instruction := range input {
--- cmd/day12/main_test.go
+++ cmd/day12/main_test.go
@@ -17,3 +17,7 @@ var input = []string{
 func TestPart1(t *testing.T) {
 	assert.Equal(t, 25, part1(input))
 }
+
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 286, part2(input))
+}
