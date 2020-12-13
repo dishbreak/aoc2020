27b20330bbc62c3223f90801250017a5a255ca71
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2020-12-12 16:05:08 -0800

    day 11 part 1

--- cmd/day11/main.go
+++ cmd/day11/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"image"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
@@ -15,8 +17,98 @@ func main() {
 	fmt.Printf("Part 1: %d\n", part1(input))
 }
 
-func part1(input []string) int {
-	result := 0
+type Game struct {
+	Board      map[image.Point]string
+	rows, cols int
+}
 
-	return result
+func NewGame(input []string) *Game {
+	board := make(map[image.Point]string)
+
+	for i, row := range input {
+		for j, symbol := range row {
+			board[image.Point{i, j}] = string(symbol)
+		}
+	}
+
+	return &Game{
+		Board: board,
+		rows:  len(input),
+		cols:  len(input[0]),
+	}
+}
+
+func neighbor8(p image.Point) []image.Point {
+	return []image.Point{
+		image.Point{p.X - 1, p.Y - 1},
+		image.Point{p.X - 1, p.Y},
+		image.Point{p.X - 1, p.Y + 1},
+		image.Point{p.X, p.Y - 1},
+		image.Point{p.X, p.Y + 1},
+		image.Point{p.X + 1, p.Y - 1},
+		image.Point{p.X + 1, p.Y},
+		image.Point{p.X + 1, p.Y + 1},
+	}
+}
+
+func (g *Game) adjacentOccupied(p image.Point) int {
+	occupied := 0
+	for _, neighbor := range neighbor8(p) {
+		if s, ok := g.Board[neighbor]; ok && s == "#" {
+			occupied++
+		}
+	}
+	return occupied
+}
+
+func (g *Game) Increment() bool {
+	changed := false
+	board := make(map[image.Point]string)
+	for k, v := range g.Board {
+		neighbors := g.adjacentOccupied(k)
+		if v == "L" && neighbors == 0 {
+			board[k] = "#"
+			changed = true
+		} else if v == "#" && neighbors > 3 {
+			board[k] = "L"
+			changed = true
+		} else {
+			board[k] = v
+		}
+	}
+
+	g.Board = board
+	return changed
+}
+
+func (g *Game) OccupiedSeats() int {
+	occupied := 0
+	for _, v := range g.Board {
+		if v == "#" {
+			occupied++
+		}
+	}
+	return occupied
+}
+
+func (g *Game) String() string {
+	var b bytes.Buffer
+	for i := 0; i < g.rows; i++ {
+		for j := 0; j < g.cols; j++ {
+			b.WriteString(g.Board[image.Point{i, j}])
+		}
+		b.WriteByte('\n')
+	}
+	return b.String()
+}
+
+func part1(input []string) int {
+	g := NewGame(input)
+
+	for i := 0; g.Increment(); i++ {
+		// fmt.Printf("--- Round %d\n", i)
+		// fmt.Println(g)
+		// fmt.Printf("Occupied: %d\n", g.OccupiedSeats())
+	}
+	return g.OccupiedSeats()
 }
