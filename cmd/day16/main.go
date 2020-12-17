3ec3da3f85f365ca899cc30f51580646204cec80
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2020-12-17 13:54:14 -0800

    day 16 part 2

--- cmd/day16/main.go
+++ cmd/day16/main.go
@@ -19,9 +19,28 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
-var rangeMatcher = regexp.MustCompile(`^([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)$`)
+func toIntSlice(input []string) ([]int, error) {
+	output := make([]int, len(input))
+	for idx, value := range input {
+		parsed, err := strconv.Atoi(value)
+		if err != nil {
+			return output, err
+		}
+		output[idx] = parsed
+	}
+	return output, nil
+}
 
-func part1(input []string) int {
+func validTicket(fieldVals []int, tree *lib.IntervalTreeNode) bool {
+	for _, fieldVal := range fieldVals {
+		if len(tree.Find(fieldVal)) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func parseRanges(input []string) ([]*lib.Range, int) {
 	counter := 0
 
 	intervals := make([]*lib.Range, 0)
@@ -29,11 +48,7 @@ func part1(input []string) int {
 		results := rangeMatcher.FindAllStringSubmatch(input[counter], -1)
 		if len(results) == 1 {
 			name := results[0][1]
-			bounds := make([]int, 4)
-			for idx, value := range results[0][2:] {
-				parsed, _ := strconv.Atoi(value)
-				bounds[idx] = parsed
-			}
+			bounds, _ := toIntSlice(results[0][2:])
 			intervals = append(
 				intervals,
 				&lib.Range{Min: bounds[0], Max: bounds[1], Metadata: name},
@@ -41,6 +56,15 @@ func part1(input []string) int {
 			)
 		}
 	}
+
+	return intervals, counter
+}
+
+var rangeMatcher = regexp.MustCompile(`^([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)$`)
+
+func part1(input []string) int {
+
+	intervals, counter := parseRanges(input)
 
 	tree, err := lib.NewIntervalTree(intervals)
 	if err != nil {
@@ -54,19 +78,14 @@ func part1(input []string) int {
 		if input[counter] == "" {
 			continue
 		}
-		parts := strings.Split(input[counter], ",")
-		for _, fieldValue := range parts {
-			parsed, err := strconv.Atoi(fieldValue)
-			if err != nil {
-				panic(err)
-			}
-			if len(tree.Find(parsed)) == 0 {
-				for _, interval := range intervals {
-					if interval.Contains(parsed) {
-						panic(fmt.Errorf("%d is in range %v", parsed, interval))
-					}
-				}
-				errorRate += parsed
+		fieldVals, err := toIntSlice(strings.Split(input[counter], ","))
+		if err != nil {
+			panic(err)
+		}
+
+		for _, fieldVal := range fieldVals {
+			if len(tree.Find(fieldVal)) == 0 {
+				errorRate += fieldVal
 			}
 		}
 	}
@@ -74,6 +93,120 @@ func part1(input []string) int {
 	return errorRate
 }
 
+func populateTicketFields(myTicket []int, validTickets [][]int, tree *lib.IntervalTreeNode) map[string]int {
+	myPopulatedTicket := make(map[string]int, len(myTicket))
+
+	fieldsVector := make([]map[string]int, len(myTicket))
+	for idx := range fieldsVector {
+		fieldsVector[idx] = make(map[string]int)
+	}
+
+	for _, ticket := range validTickets {
+		for idx, fieldVal := range ticket {
+			matchingIntervals := tree.Find(fieldVal)
+			fieldHits := make(map[string]int, len(myTicket))
+			for j := range matchingIntervals {
+				if fieldName, ok := matchingIntervals[j].Metadata.(string); ok {
+					fieldsVector[idx][fieldName]++
+					fieldHits[fieldName]++
+				}
+			}
+
+			for k, v := range fieldHits {
+				if v > 1 {
+					panic(fmt.Errorf("%d matched '%s' %d times", fieldVal, k, v))
+				}
+			}
+		}
+	}
+
+	// we're going to play multiple rounds of the following
+	for len(myPopulatedTicket) != len(myTicket) {
+		// find fields that match all values in a given position
+		for idx, state := range fieldsVector {
+			// if a state vector entry is nil it means we've locked that
+			// position to a given field.
+
+			if state == nil {
+				continue
+			}
+			var fieldCandidate string
+			matches := 0
+			for field, hits := range state {
+				if hits == len(validTickets) {
+					fieldCandidate = field
+					matches++
+				}
+			}
+			// if only one field matched all values for this ticket, we can lock
+			// in this position as matching to that field
+			if matches == 1 {
+				myPopulatedTicket[fieldCandidate] = myTicket[idx]
+				fieldsVector[idx] = nil
+			}
+		}
+
+		// eliminate locked in fields from the remaining state vectors
+		for _, state := range fieldsVector {
+			if state == nil {
+				continue
+			}
+			for k := range myPopulatedTicket {
+				delete(state, k)
+			}
+		}
+
+	}
+
+	return myPopulatedTicket
+}
+
 func part2(input []string) int {
-	return 0
+	intervals, counter := parseRanges(input)
+
+	tree, err := lib.NewIntervalTree(intervals)
+	if err != nil {
+		panic(err)
+	}
+
+	counter += 2
+
+	myTicketValues, err := toIntSlice(strings.Split(input[counter], ","))
+	if err != nil {
+		panic(err)
+	}
+
+	counter += 3
+	validTickets := make([][]int, 0)
+
+	for ; counter < len(input); counter++ {
+		if input[counter] == "" {
+			continue
+		}
+
+		ticket, err := toIntSlice(strings.Split(input[counter], ","))
+		if err != nil {
+			panic(err)
+		}
+		if validTicket(ticket, tree) {
+			validTickets = append(validTickets, ticket)
+		}
+	}
+
+	myTicketFields := populateTicketFields(myTicketValues, validTickets, tree)
+
+	result := 1
+	i := 0
+	for k, v := range myTicketFields {
+		if strings.HasPrefix(k, "departure") {
+			result *= v
+			i++
+		}
+	}
+
+	if i != 6 {
+		panic(fmt.Errorf("failed to find 6 fields starting with departure"))
+	}
+
+	return result
 }
