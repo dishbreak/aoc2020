f241955925cb8353fe7a0fe061f19efd1075af57
Vishal Kotcherlakota <vkotcherlakota@nerdwallet.com>
2021-11-18 13:54:47 -0800

    day 25 complete

--- cmd/day25/main_test.go
+++ cmd/day25/main_test.go
@@ -7,12 +7,11 @@ import (
 )
 
 func TestGetLoopSize(t *testing.T) {
-	loopSize, err := getLoopSize(5764801)
+	loopSize := getLoopSize(5764801)
 	assert.Equal(t, 8, loopSize)
-	assert.Nil(t, err)
 }
 
 func TestGetEncKey(t *testing.T) {
-	assert.Equal(t, 5764801, getEncKey(1, 8))
 	assert.Equal(t, 14897079, getEncKey(17807724, 8))
+	assert.Equal(t, 14897079, getEncKey(5764801, 11))
 }
